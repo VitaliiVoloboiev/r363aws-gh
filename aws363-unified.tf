3c70c33ae5857f3d89dad3c68501006d81e4caeb
Vitalii <83081678+VitaliiVoloboiev@users.noreply.github.com>
2022-07-06 15:58:49 +0300

    Update aws363-unified.tf

--- aws363-unified.tf
+++ aws363-unified.tf
@@ -1,10 +1,10 @@
 provider "m3" {}
 resource "m3_instance" "my-server" {
 image = "Ubuntu20.04_64-bit"
-instance_name = "regr363-aws-uni"
+instance_name = "vtfunifromgh"
 region_name = "AWS-EUWEST"
 tenant_name = "EPM-CIT2"
 shape = "MICRO"
-key_name = "created-for-all-regr"
+key_name = "vvv-5890-Alll-ssh"
 owner = "vitalii_voloboiev@epam.com"
 }
